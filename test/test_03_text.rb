a5c6f127e63b9214e91071e42167abf92b32506e
Mike Owens <mike@filespanker.com>
2014-01-26 07:01:05 -0500

    assert_screens -> assert_screen in tests

--- test/test_03_text.rb
+++ test/test_03_text.rb
@@ -10,25 +10,28 @@ require 'term/vt102'
 
 class TestText < TestBase
   def test_originals
-    assert_screens [
-      [ 10, 5, "line 1\r\n  line 2\r\n  line 3\r\nline 4",
+    assert_screen [
+        10, 5, "line 1\r\n  line 2\r\n  line 3\r\nline 4",
         "line 1" + ("\0" * 4),
         "  line 2" + ("\0" * 2),
         "  line 3" + ("\0" * 2),
         "line 4" + ("\0" * 4),
-      ],
-      [ 80, 25, " line 1 \n    line 2\n    line 3\n line 4 ",
+      ]
+
+    assert_screen [
+        80, 25, " line 1 \n    line 2\n    line 3\n line 4 ",
         " line 1 " + ("\0" * 72),
         ("\0" * 8) + "    line 2" + ("\0" * 62),
         ("\0" * 18) + "    line 3" + ("\0" * 52),
         ("\0" * 28) + " line 4 " + ("\0" * 44),
-      ],
-      [ 40, 5, "line 1\ttab 1\r\n  line 2\ttab 2\ttab 3\r\n  line 3\r\nline 4",
+      ]
+
+    assert_screen [
+      40, 5, "line 1\ttab 1\r\n  line 2\ttab 2\ttab 3\r\n  line 3\r\nline 4",
         "line 1\0\0tab 1" + ("\0" * 27),
         "  line 2\0\0\0\0\0\0\0\0tab 2\0\0\0tab 3" + ("\0" * 11),
         "  line 3" + ("\0" * 32),
         "line 4" + ("\0" * 34),
-      ],
-    ];
+      ]
   end
 end
--- test/test_13_xonxoff.rb
+++ test/test_13_xonxoff.rb
@@ -11,8 +11,8 @@ require 'term/vt102'
 class TestXonxoff < TestBase
   def test_00_xon_xoff
     #                 (F,B,b,f,s,u,F,r)
-    assert_screens [
-      [ { 'IGNOREXOFF' => 0 },
+    assert_screen [
+        { 'IGNOREXOFF' => 0 },
           6, 2, "foo\023bar\e[1m\021baz",
           "foobaz", [ [7,0,0,0,0,0,0,0],
                       [7,0,0,0,0,0,0,0],
@@ -27,8 +27,10 @@ class TestXonxoff < TestBase
                      [7,0,0,0,0,0,0,0],
                      [7,0,0,0,0,0,0,0],
                      [7,0,0,0,0,0,0,0] ],
-        ],
-        [ { 'IGNOREXOFF' => 1 },
+        ]
+
+    assert_screen [
+         { 'IGNOREXOFF' => 1 },
           9, 1, "foo\023bar\e[1m\021baz",
           "foobarbaz",
                     [ [7,0,0,0,0,0,0,0],
@@ -41,7 +43,6 @@ class TestXonxoff < TestBase
                       [7,0,1,0,0,0,0,0],
                       [7,0,1,0,0,0,0,0] ],
         ]
-    ]
   end
 end
 
